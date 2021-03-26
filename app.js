7fd4b232dfa3eda96560d85ecb72e5bbdc932c99
Gabriel Simek <gabrielsimek@outlook.com>
2021-03-25 17:31:45 -0700

    finished logic

--- app.js
+++ app.js
@@ -14,15 +14,65 @@ const totalLossesEl = document.getElementById('total-losses');
 let totalGames = 0;
 let totalWins = 0;
 let totalDraws = 0;
+
+
+
 // set event listeners to update state and DOM
 
 playButton.addEventListener('click', () => {
     totalGames++;
-    
-    let randomNum = Math.floor(Math.random() * 3);
-    console.log(getComputerChoice(randomNum));
-    
 
+    let randomNum = Math.floor(Math.random() * 3);
+
+    let computerChoice = getComputerChoice(randomNum);
+
+    const currentlySelectedButton = document.querySelector('input:checked');
+
+    let userChoice = currentlySelectedButton.value;
+
+    console.log(userChoice, computerChoice);
+    function declareWinner(userChoice, computerChoice) {
+
+        if (userChoice === computerChoice) {
+            totalDraws++;
+            resultEl.textContent = 'Tie';
+        } else if (userChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                resultEl.textContent = 'You lose!';
+            } else {
+                totalWins++;
+                resultEl.textContent = 'You win!';
+            }
+        } else if (userChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                resultEl.textContent = 'You lose!';
+            } else {
+                totalWins++;
+                resultEl.textContent = 'You win!';
+            }
+        } else if (userChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                resultEl.textContent = 'You lose!';
+            } else {
+                totalWins++;
+                resultEl.textContent = 'You win!';
+            }
+        }
+    }
+
+
+
+    declareWinner(userChoice, computerChoice);
+
+
+
+
+
+    totalDrawsEl.textContent = totalDraws;
+    totalWinsEl.textContent = totalWins;
+    totalGamesEl.textContent = totalGames;
+    computerChoiceEl.textContent = computerChoice;
+    totalLossesEl.textContent = totalGames - totalDraws - totalWins;
 
 
 });
