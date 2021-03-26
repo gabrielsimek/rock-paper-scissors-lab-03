4cf7c8f57e542224f2319fe2ed592738297b6cec
Gabriel Simek <gabrielsimek@outlook.com>
2021-03-25 22:04:09 -0700

    add reset button and hide computer player before game starts

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const totalGamesEl = document.getElementById('total-games');
 const totalWinsEl = document.getElementById('total-wins');
 const totalDrawsEl = document.getElementById('total-draws');
 const totalLossesEl = document.getElementById('total-losses');
-
-
+const resetButton = document.querySelector('#reset-button');
+const hidden = document.querySelector('.hidden')
+console.log(hidden);
 
 // initialize state
 let totalGames = 0;
@@ -21,6 +22,7 @@ let totalDraws = 0;
 
 playButton.addEventListener('click', () => {
     totalGames++;
+    hidden.classList.remove('hidden')
 
     let randomNum = Math.floor(Math.random() * 3);
     // console.log(randomNum);
@@ -55,4 +57,18 @@ playButton.addEventListener('click', () => {
     totalLossesEl.textContent = totalGames - totalDraws - totalWins;
 
 
+});
+
+resetButton.addEventListener('click', () => {
+    totalGames = 0;
+    totalWins = 0;
+    totalDraws = 0;
+
+    totalDrawsEl.textContent = totalDraws;
+    totalWinsEl.textContent = totalWins;
+    totalGamesEl.textContent = totalGames;
+    computerChoiceEl.textContent = '';
+    totalLossesEl.textContent = 0;
+
+    hidden.classList.add('hidden')
 });
--- utils.js
+++ utils.js
@@ -33,3 +33,4 @@ export function declareWinner(userChoice, computerChoice) {
         }
     }
 }
+
